faa71ee234982f051fdc62ac801399f031e01fde
hilla <kipronohillary240@gmail.com>
2022-04-02 03:08:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/registerlogin/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.registerlogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+       
+    }
+}
